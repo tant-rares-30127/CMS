8f0f55af2f9becb598fe6b3732d5a0643b8dca8d
Rares Tant <79656115+tant-rares-30127@users.noreply.github.com>
2021-09-06 10:19:17 +0300

    Adding delete button (style)

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 function AddMember() {
-        var lastName=document.getElementById('lastNameInput').value;
-        var firstName=document.getElementById('firstNameInput').value;
-        var email=document.getElementById('emailInput').value;
-        var sex=document.getElementById('sexInput').value;
-        var birthdate=document.getElementById('birthdateInput').value;
-        console.log(`${lastName} ${firstName} ${email} ${sex} ${birthdate}`);
-        var table=document.getElementById('table');
-        var row=table.insertRow();
-        var cell1=row.insertCell(0);
-        var cell2=row.insertCell(1);
-        var cell3=row.insertCell(2);
-        var cell4=row.insertCell(3);
-        var cell5=row.insertCell(4);
-        cell1.innerHTML=lastName;
-        cell2.innerHTML=firstName;
-        cell3.innerHTML=email;
-        cell4.innerHTML=sex;
-        cell5.innerHTML=birthdate;
-    }
+    var lastName=document.getElementById('lastNameInput').value;
+    var firstName=document.getElementById('firstNameInput').value;
+    var email=document.getElementById('emailInput').value;
+    var sex=document.getElementById('sexInput').value;
+    var birthdate=document.getElementById('birthdateInput').value;
+    console.log(`${lastName} ${firstName} ${email} ${sex} ${birthdate}`);
+    var table=document.getElementById('table');
+    var row=table.insertRow();
+    var cell1=row.insertCell(0);
+    var cell2=row.insertCell(1);
+    var cell3=row.insertCell(2);
+    var cell4=row.insertCell(3);
+    var cell5=row.insertCell(4);
+    var cell6=row.insertCell(5);
+    cell1.innerHTML=lastName;
+    cell2.innerHTML=firstName;
+    cell3.innerHTML=email;
+    cell4.innerHTML=sex;
+    cell5.innerHTML=birthdate; 
+    cell6.innerHTML='<button class="delete-button" id="deleteButton" onclick="DeleteMember()">Delete</button>'
+}
+
+function DeleteMember(){
+
+}
