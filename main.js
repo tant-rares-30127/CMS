cab5d0bc63e396e780b971ea269abd882cfc6080
Rares Tant <79656115+tant-rares-30127@users.noreply.github.com>
2021-09-07 09:56:37 +0300

    Fixing 2 click bug (thx Claudia)

--- main.js
+++ main.js
@@ -100,9 +100,7 @@ function OpenModal(){
     var modal = document.getElementById("myModal");
     var btn = document.getElementById("myBtn");
     var span = document.getElementsByClassName("close")[0];
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
+    modal.style.display = "block";
     span.onclick = function() {
         modal.style.display = "none";
     }
