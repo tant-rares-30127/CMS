a8732a2ced994d03733b3734111f115f78d340e3
Rares Tant <79656115+tant-rares-30127@users.noreply.github.com>
2021-09-10 11:28:22 +0300

    Merged index.js with main.js+updating add function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js";
-import {getFirestore, doc, setDoc, getDoc, getDocs, collection} from "https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js";
+import {getFirestore, doc, setDoc, getDoc, getDocs, collection, Timestamp} from "https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDxqcFls44cCSrDK60cbr6aBZh6ioImRtY",
@@ -15,16 +15,164 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 const db=getFirestore(firebaseApp);
 const querySnapshot = await getDocs(collection(db, "members"));
+const membersRef = collection(db, "members");
 
-function WriteMember(){
-  var i=0;
+function  ReadMembersFromDatabase(){
   querySnapshot.forEach(element => {
-    console.log(element.data()["first-name"]);
-    console.log(element.data()["last-name"]);
-    console.log(element.data()["email"]);
-    console.log(element.data()["sex"]);
-    console.log(element.data()["birthdate"].toDate());
+    var lastName=element.data()["lastname"];
+    var firstName=element.data()["firstname"];
+    var email=element.data()["email"];
+    var sex=element.data()["sex"];
+    var date=element.data()["birthdate"].toDate();
+    var year=date.getFullYear();
+    var month=date.getMonth() + 1;
+    var day=date.getDate();
+    var birthdate=year + "/" + month + "/" + day;
+    var table=document.getElementById('table');
+    var row=table.insertRow();
+    row.className="bottom-row";
+    var cell1=row.insertCell(0);
+    var cell2=row.insertCell(1);
+    var cell3=row.insertCell(2);
+    var cell4=row.insertCell(3);
+    var cell5=row.insertCell(4);
+    var cell6=row.insertCell(5);
+    cell1.innerHTML=lastName;
+    cell2.innerHTML=firstName;
+    cell3.innerHTML=email;
+    cell4.innerHTML=sex;
+    cell5.innerHTML=birthdate; 
+    cell6.innerHTML='<span class="delete-button fa fa-remove" id="deleteButton" onclick="DeleteMember(this)">'
   });
 }
 
-WriteMember();
+ReadMembersFromDatabase();
+
+function AddMemberInDatabase(lastName, firstName, Email, Sex, Birthdate){
+  var currentid=document.getElementById('table').rows.length-1;
+  console.log(currentid);
+  setDoc(doc(membersRef),{
+    firstname: lastName,
+    lastname: firstName,
+    email: Email,
+    sex: Sex,
+    birthdate: Timestamp.fromDate(Birthdate),
+    id: currentid
+  });
+}
+
+document.getElementById("addButton").addEventListener("click", async function(){
+  AddMember();
+});
+
+function AddMember() {
+    var lastName=document.getElementById('lastNameInput').value;
+    var firstName=document.getElementById('firstNameInput').value;
+    var email=document.getElementById('emailInput').value;
+    var sex=document.getElementById('sexInput').value;
+    var birthdate=document.getElementById('birthdateInput').value;
+    var boolean=0;
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (lastName==""){
+        alert("Last name is empty");
+        boolean=1;
+    }
+    if (firstName==""){
+        alert("First name is empty");
+        boolean=1;
+    }
+    if (email==""){
+        alert("Email is empty");
+        boolean=1;
+    }
+    if (sex==""){
+        alert("Choose your sex");
+        boolean=1;
+    }
+    if (birthdate==""){
+        alert("Choose a birthdate");
+        boolean=1;
+    }
+
+    if (regex.test(String(email).toLowerCase())){
+    }
+    else{
+        alert("Insert a valid email");
+        boolean=1;
+    }
+
+    var dtToday = new Date();
+    var year = dtToday.getFullYear();
+    var month = dtToday.getMonth();
+    var day = dtToday.getDate();
+    var selectedData=document.getElementById("birthdateInput").value;
+    var selectedDate=new Date(selectedData);
+    var selectedYear=selectedDate.getFullYear();
+    var selectedMonth=selectedDate.getMonth();
+    var selectedDay=selectedDate.getDate();
+
+    if (year-selectedYear<=16){
+        if (year-selectedYear==16){
+            if (selectedMonth>=month)
+                if (selectedMonth==month){
+                    if (selectedDay>day){
+                        boolean=1;
+                        alert("You have to be at least 16 years old");
+                    }
+                }
+                else{
+                    boolean=1;
+                    alert("You have to be at least 16 years old");
+                }
+        }
+        else{
+            boolean=1;
+            alert("You have to be at least 16 years old");
+        }
+    } 
+
+    if (boolean==0){
+        var table=document.getElementById('table');
+        var row=table.insertRow();
+        row.className="bottom-row";
+        var cell1=row.insertCell(0);
+        var cell2=row.insertCell(1);
+        var cell3=row.insertCell(2);
+        var cell4=row.insertCell(3);
+        var cell5=row.insertCell(4);
+        var cell6=row.insertCell(5);
+        cell1.innerHTML=lastName;
+        cell2.innerHTML=firstName;
+        cell3.innerHTML=email;
+        cell4.innerHTML=sex;
+        cell5.innerHTML=birthdate; 
+        cell6.innerHTML='<span class="delete-button fa fa-remove" id="deleteButton" onclick="DeleteMember(this)">'
+        AddMemberInDatabase(lastName, firstName, email, sex, selectedDate);
+    }
+}
+
+function DeleteMember(row){
+    var table=document.getElementById('table');
+    var id=row.parentNode.parentNode.rowIndex;
+    table.deleteRow(id);
+}
+
+document.getElementById("myBtn").addEventListener("click", async function(){
+  OpenModal();
+});
+
+function OpenModal(){
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+    modal.style.display = "block";
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
